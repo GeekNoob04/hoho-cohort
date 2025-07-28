f61d5ed9cf5dbc2c2f1bb901d2169ff73e6574db
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-07-28 13:40:16 +0530

    get requests in hono

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
+app.get("/", async (c) => {
+    const body = await c.req.json();
+    console.log(body);
+    console.log(c.req.header("Authorization"));
+    console.log(c.req.query("param"));
     return c.json({
         message: "Hello Hono!",
     });
